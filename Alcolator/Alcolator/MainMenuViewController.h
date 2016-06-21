df9779539a2765066356ce0bb233a632791abcd9
Daman Kapoor <daman_kapoor@yahoo.com>
2016-06-20 20:22:23 -0700

    Final Commit

---
+++ Alcolator/Alcolator/MainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.h
+//  Alcolator
+//
+//  Created by Daman Kapoor on 6/20/16.
+//  Copyright © 2016 Wandersolo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface MainMenuViewController : ViewController
+
+@end
